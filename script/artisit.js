1a372c0b42c205eb9b4a38c6c9b50abbd05f058d
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-01-23 12:09:33 +0100

    added js

---
+++ script/artisit.js
@@ -0,0 +1,82 @@
+// getting reference of elements to fill
+const artistCover = document.getElementById("artist-cover");
+const artistName = document.getElementById("artist-name");
+const artistListeners = document.getElementById("nb-fan");
+const colInside = document.getElementById("traccia-album");
+
+//funzione svuota col
+const emptyCol = () => {
+  colInside.innerHTML = ``;
+};
+//funzione tracklist
+const gettingTracks = (tracks) => {
+  tracks.forEach((track, i) => {
+    // emptyCol();
+    const newCol = document.createElement("div");
+    newCol.classList.add(
+      "col-8",
+      "m-1",
+      "p-1",
+      "d-flex",
+      "justify-content-evenly",
+      "align-items-center",
+      "flex-wrap",
+      "flex-row"
+    );
+    newCol.innerHTML = `
+              <h3>${i + 1}</h3>
+            <img src="${
+              track.album.cover_small
+            }" class="img-thumbnail" alt="album-cover">
+            <h4>${track.title_short}</h4>
+            <i class="bi bi-three-dots-vertical text-fontB50 fs-1 d-sm-none"></i>
+            <h5>${track.rank}</h5>
+            <h6 class="d-none d-sm-block">${track.duration}</h6>
+          </div>`;
+    colInside.appendChild(newCol);
+  });
+};
+
+// riempio dinamicamente la pagina
+const artistURL = "https://striveschool-api.herokuapp.com/api/deezer/artist";
+const addressBar = new URLSearchParams(location.search);
+// const artistID= addressBar.get('artistID') questa la commento per il momento e assegno alla const un valore statico
+const artistID = 412;
+fetch(`${artistURL}/${artistID}`)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response.status);
+    }
+  })
+  .then((artist) => {
+    console.log(artist);
+    artistCover.style.backgroundImage = `url(${artist.picture_xl})`;
+    artistCover.style.backgroundRepeat = "no-repeat";
+    artistCover.style.backgroundSize = "cover";
+    artistName.innerText = artist.name;
+    artistListeners.innerText = artist.nb_fan;
+
+    // faccio fetch dell'api nell'api
+    const artistTracklist = artist.tracklist;
+    console.log(artistTracklist);
+    fetch(artistTracklist)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.status);
+        }
+      })
+      //ora posso lavorare con l'array delle tracce
+      .then((tracklist) => {
+        console.log(tracklist.data);
+        gettingTracks(tracklist.data);
+      });
+  })
+
+  // qui gestisco l'errore che ho'gettato' nell'else
+  .catch((err) => {
+    alert(err);
+  });
