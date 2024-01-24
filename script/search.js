a02fa64547070c8470d396526d3ca10dcea5efe8
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-01-24 16:39:02 +0100

    display cards

--- script/search.js
+++ script/search.js
@@ -32,7 +32,9 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .then((data) => {
           console.log(data);
-          displaySearchResults(data.data);
+          const firstFiveResult = data.data.slice(0, 5);
+          console.log("il risultato è:", firstFiveResult);
+          displaySearchResults(firstFiveResult);
         })
         .catch((err) => {
           console.log("Errore durante la ricerca:", err);
@@ -45,75 +47,147 @@ document.addEventListener("DOMContentLoaded", function () {
     const query = searchInput.value.trim();
     const formattedQuery = toTitleCase(query);
     console.log(formattedQuery);
-    results.forEach((result) => {
-      const resultItem = document.createElement("div");
-      resultItem.classList.add("result-item", "col-6", "col-md-2");
 
-      if (formattedQuery) {
-        resultItem.innerHTML = `
-        <div class="col-6">
-          <h2>Risultato più rilevante</h2>
-          <div class="row" id="relevant">
-            <div class="col-6 bg-tertiary">
-            </div>
+    // recupera il primo risultato
+    const firstResult = results[0];
+
+    // crea la col-6 per il relevant
+    const colRelevant = document.createElement("div");
+    colRelevant.classList.add(
+      "col-8",
+      "col-lg-6",
+      "col-xl-4",
+      "h-100",
+      "results",
+      "mt-3"
+    );
+
+    // verifica se la formattedQuery è uguale al nome dell'artista
+    if (formattedQuery === firstResult.artist.name) {
+      console.log("nome artista:", firstResult.artist.name);
+      colRelevant.innerHTML = `
+      <h2>Risultato più rilevante</h2>
+      <div class="row d-flex flex-column bg-tertiary rounded p-3 ">
+        <div class="col-8">
+          <div class="d-flex flex-column justify-content-around">
+            <img src="${firstResult.artist.picture_xl}" class="img-fluid w-50 rounded-circle shadow mb-3" alt="...">
+            <h3 class="card-title">${firstResult.artist.name}</h3>
+            <p class="card-text text-fontB50">${firstResult.artist.type}</p>
           </div>
         </div>
-        <div class="col-6">
-          <h2>Brani</h2>
-          <div class="row" id="tracks">
-            <div class="col"></div>
-          </div>
-        </div>
-        `;
-      }
-
-      const rowRelevant = document.getElementById("relevant");
-      const rowTracks = document.getElementById("tracks");
-
-      if (formattedQuery === result.artist.name) {
-        resultItem.innerHTML = `
-      <div class="card">
-          <img src="${result.artist.picture_big}" class="card-img-top" alt="...">
-          <div class="card-body">
-              <h5 class="card-title">${result.artist.name}</h5>
-              <p class="card-text">${result.artist.type}</p>
-          </div>
       </div>
+      `;
+    } else if (formattedQuery === firstResult.album.title) {
+      console.log("nome album:", firstResult.album.title);
+      colRelevant.innerHTML = `
+      <h2>Risultato più rilevante</h2>
+      <div class="row d-flex flex-column bg-tertiary rounded p-3 ">
+        <div class="col-10">
+          <div class="d-flex flex-column justify-content-around">
+            <img src="${firstResult.album.cover_medium}" class="img-fluid w-50 rounded shadow mb-3" alt="...">
+            <h3 class="card-title">${firstResult.album.title}</h3>
+            <p class="card-text text-fontB50">${firstResult.album.type}<i class="bi bi-dot"></i>${firstResult.artist.name}</p>
+          </div>
+        </div>
+      </div>
+      `;
+    } else if (formattedQuery === firstResult.title) {
+      console.log("nome traccia:", firstResult.title);
+      colRelevant.innerHTML = `
+      <h2>Risultato più rilevante</h2>
+      <div class="row d-flex flex-column bg-tertiary rounded p-3 ">
+        <div class="col-10">
+          <div class="d-flex flex-column justify-content-around">
+            <img src="${firstResult.album.cover_medium}" class="img-fluid w-50 rounded shadow mb-3" alt="...">
+            <h3 class="card-title">${firstResult.title}</h3>
+            <p class="card-text text-fontB50">${firstResult.type}<i class="bi bi-dot"></i>${firstResult.artist.name}</p>
+          </div>
+        </div>
+      </div>
+      `;
+    }
+    resultSection.appendChild(colRelevant);
+
+    const colTracks = document.createElement("div");
+    colTracks.classList.add("col-12", "col-lg-6", "col-xl-8", "h-100", "mt-3");
+    colTracks.innerHTML = `
+      <h2>Brani</h2>
     `;
-      } else if (formattedQuery === result.album.title) {
-        resultItem.innerHTML = `
-        <div class="card">
-            <img src="${result.album.cover_xl}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${result.album.title}[0]</h5>
-                <p class="card-text">${result.album.type}<span><i class="bi bi-dot"></i></span><strong>${result.artist.name}</strong></p>
-            </div>
-        </div>
-      `;
-      } else if (formattedQuery === result.title) {
-        resultItem.innerHTML = `
-        <div class="card">
-            <img src="${result.album.cover_xl}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${result.title}</h5>
-                <p class="card-text">${result.type}<span><i class="bi bi-dot"></i></span><strong>${result.artist.name}</strong></p>
-            </div>
-        </div>
-      `;
-      }
+    resultSection.appendChild(colTracks);
+    const rowTracks = document.createElement("div");
+    rowTracks.classList.add("row", "results", "gy-2");
+    colTracks.appendChild(rowTracks);
 
-      // resultItem.innerHTML = `
-      //         <div class="card">
-      //             <img src="${result.album.cover_big}" class="card-img-top" alt="...">
-      //             <div class="card-body">
-      //                 <h5 class="card-title">${result.title}</h5>
-      //                 <p class="card-text">${result.type}<span><i class="bi bi-dot"></i></span><strong>${result.artist.name}</strong></p>
-      //             </div>
-      //         </div>
-      //       `;
+    // funzione per formattare in minuti:secondi
 
-      resultSection.appendChild(resultItem);
-    });
+    const convertTime = function (secondiStringa) {
+      const seconds = parseInt(secondiStringa, 10);
+      const minutes = Math.floor(seconds / 60);
+      const secondsRemains = seconds % 60;
+      return `${minutes}:${secondsRemains < 10 ? "0" : ""}${secondsRemains}`;
+    };
+
+    for (let i = 1; i < results.length; i++) {
+      const colTrack = document.createElement("div");
+      colTrack.classList.add("col-12");
+      colTrack.innerHTML = `
+      <div class="row">
+       <div class="col-2">
+          <img src="${results[i].album.cover_small}" class="img-fluid rounded">
+        </div>
+        <div class="col-8">
+          <div class="row">
+            <div class="col-12">
+              <h6 class="mb-0">${results[i].title}</h6>
+            </div>
+            <div class="col-12 text-fontB50">
+              <p class="mb-0">${results[i].artist.name}</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2 text-fontB50">${convertTime(
+          results[i].duration
+        )}</div>
+      </div>
+      `;
+      rowTracks.appendChild(colTrack);
+    }
   };
   searchInput.addEventListener("input", handleSearch);
+});
+
+// funzione per mostrare le card all'avvio della pagina
+
+const arrayOfImages = [
+  "./assets/imgs/search/image-1.jpeg",
+  "./assets/imgs/search/image-2.jpg",
+  "./assets/imgs/search/image-3.jpg",
+  "./assets/imgs/search/image-4.jpg",
+  "./assets/imgs/search/image-5.jpg",
+  "./assets/imgs/search/image-6.jpg",
+  "./assets/imgs/search/image-7.jpg",
+  "./assets/imgs/search/image-8.jpg",
+  "./assets/imgs/search/image-9.jpg",
+  "./assets/imgs/search/image-10.jpg",
+  "./assets/imgs/search/image-11.jpg",
+];
+const rowToDisplay = document.getElementById("rowSfoglia");
+
+arrayOfImages.forEach((imageSrc) => {
+  const divElement = document.createElement("div");
+  divElement.classList.add("col-6", "col-md-4", "col-lg-2");
+  // divElement.id = "playlist";
+  divElement.innerHTML = `
+  <div style="overflow:hidden"  class="bg-primary rounded p-2 h-100">
+    <h3 class="mb-5">Musica</h3>
+    <div class="playlist h-100 text-end">
+      <img
+        src="${imageSrc}"
+        class="w-75 shadow"
+      />
+    </div>
+  </div>
+  `;
+
+  rowToDisplay.appendChild(divElement);
 });
