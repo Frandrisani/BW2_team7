0666e77dbc7141938eba6ac690ee68bdfddc3541
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-01-25 10:57:17 +0100

    i

--- script/artisti.js
+++ script/artisti.js
@@ -81,7 +81,7 @@ const gettingTracks = (tracks) => {
 // riempio dinamicamente la pagina
 const artistURL = "https://striveschool-api.herokuapp.com/api/deezer/artist";
 const addressBar = new URLSearchParams(location.search);
-// const artistID= addressBar.get('artistID') questa la commento per il momento e assegno alla const un valore statico
+// const artistID= addressBar.get('artistID')
 const artistID = 412;
 fetch(`${artistURL}/${artistID}`)
   .then((response) => {
